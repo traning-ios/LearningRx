87f3c0d174e869a623b478bc9106245117ff0ea5
quynhlx <quynhlxbkhn@gmail.com>
2019-11-03 21:40:52 +0900

    Initial Commit

---
+++ SearchNameTests/SearchNameTests.swift
@@ -0,0 +1,34 @@
+//
+//  SearchNameTests.swift
+//  SearchNameTests
+//
+//  Created by quynhlx on 2019/11/03.
+//  Copyright © 2019 quynhlx. All rights reserved.
+//
+
+import XCTest
+@testable import SearchName
+
+class SearchNameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
